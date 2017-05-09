2f46811872e0016e7fe771bf2eb3c2e54089383b
Ryan Bragg <ryan.a.bragg@gmail.com>
2017-05-08 22:23:36 -0400

    corrected import of PageNotFound

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import Homepage from './Homepage';
-import {PageNotFound} from './PageNotFound';
+import PageNotFound from './PageNotFound';
 
 export default class App extends React.Component {
   render() {
@@ -9,7 +9,7 @@ export default class App extends React.Component {
       <div className="route-container">
         <Switch>
           <Route exact path='/' component={Homepage}/>
-          <Route path='/admin' component={PageNotFound}/>
+          <Route exact path='/admin' component={PageNotFound}/>
           <Route path='*' component={PageNotFound}/>
         </Switch>
       </div>
