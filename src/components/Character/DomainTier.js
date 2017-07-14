bd14e37360648ceab0c1c1c145c7ed6d649ecebb
Ryan Bragg <ryan.a.bragg@gmail.com>
2017-07-14 13:25:45 -0400

    added DomainTier component

---
+++ src/components/Character/DomainTier.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Ability from './Ability';
+
+class DomainTier extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div data-character='tier'>
+        <span data-character='tier-label'>Tier {this.props.tier}</span>
+        {this.props.abilities.map(ability => {
+          return (
+            <Ability
+              key={ability.id}
+              id={ability.id}
+              name={ability.name}
+              display={ability.display}
+              count= {ability.count}
+              viewDescription={this.props.viewDescription}
+              updateCharacterAbility={this.props.updateCharacterAbility}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+//DomainTier.defaultProps = {};
+
+DomainTier.propTypes = {
+  domain: PropTypes.string.isRequired,
+  tier: PropTypes.number.isRequired,
+  abilities: PropTypes.array.isRequired,
+  viewDescription: PropTypes.func.isRequired,
+  updateCharacterAbility: PropTypes.func.isRequired
+};
+
+export default DomainTier;
---
+++ test/components/Character/DomainTier.test.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { expect } from 'chai';
+import { spy } from 'sinon';
+import { shallow } from 'enzyme';
+import { JSDOM } from 'jsdom';
+
+import DomainTier from '../../../src/components/Character/DomainTier';
+import Ability from '../../../src/components/Character/Ability';
+
+const window = (new JSDOM('<!doctype html><html><body></body></html>')).window;
+global.window = window;
+global.document = window.document;
+global.navigator = {
+  userAgent: 'node.js',
+};
+
+function copyProps(src, target) {
+  const props = Object.getOwnPropertyNames(src)
+    .filter(prop => typeof target[prop] === 'undefined')
+    .map(prop => Object.getOwnPropertyDescriptor(src, prop));
+  Object.defineProperties(target, props);
+}
+copyProps(window, global);
+
+describe('<DomainTier />', () => {
+  it('renders a div with a span and an Ability component for each object in the abilities prop', () => {
+    const view = spy(), update = spy();
+    let list = [{
+      id: 42,
+      name: 'test'
+    }];
+    const wrapper = shallow(<DomainTier domain='test' tier={1} abilities={list} viewDescription={view} updateCharacterAbility={update}/>);
+    expect(wrapper.find('div')).to.have.length(1);
+    expect(wrapper.find('div').childAt(0).type()).to.equal('span');
+    expect(wrapper.find('span')).to.have.length(1);
+    expect(wrapper.find('div').childAt(1).type()).to.equal(Ability);
+    expect(wrapper.find(Ability)).to.have.length(list.length);
+    list = list.concat([{
+      id: 7,
+      name: 'lucky',
+      display: 'checkbox',
+      count: 11
+    }, {
+      id: 0,
+      name: 'choice',
+      display: 'tiers',
+      count: 1
+    }]);
+    wrapper.setProps({abilities: list});
+    expect(wrapper.find(Ability)).to.have.length(list.length);
+  });
+
+  it('renders a Ability component with the color prop based on the passed stones prop', () => {
+    const view = spy(), update = spy();
+    const list = [{
+      id: 42,
+      name: 'test'
+    }, {
+      id: 7,
+      name: 'lucky',
+      display: 'checkbox',
+      count: 11
+    }];
+    const wrapper = shallow(<DomainTier domain='test' tier={1} abilities={list} viewDescription={view} updateCharacterAbility={update}/>);
+    expect(wrapper.find(Ability).at(0).prop('id')).to.equal(list[0].id);
+    expect(wrapper.find(Ability).at(0).prop('name')).to.equal(list[0].name);
+    expect(wrapper.find(Ability).at(0).prop('display')).to.equal(undefined);
+    expect(wrapper.find(Ability).at(0).prop('count')).to.equal(0);
+    expect(wrapper.find(Ability).at(0).prop('viewDescription')).to.equal(view);
+    expect(wrapper.find(Ability).at(0).prop('updateCharacterAbility')).to.equal(update);
+    expect(wrapper.find(Ability).at(1).prop('id')).to.equal(list[1].id);
+    expect(wrapper.find(Ability).at(1).prop('name')).to.equal(list[1].name);
+    expect(wrapper.find(Ability).at(1).prop('display')).to.equal(list[1].display);
+    expect(wrapper.find(Ability).at(1).prop('count')).to.equal(list[1].count);
+    expect(wrapper.find(Ability).at(1).prop('viewDescription')).to.equal(view);
+    expect(wrapper.find(Ability).at(1).prop('updateCharacterAbility')).to.equal(update);
+  });
+});
