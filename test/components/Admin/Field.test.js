cc472abac6f0480c91a78f5a23bc4dd34ce5faf8
Ryan Bragg <ryan.a.bragg@gmail.com>
2017-07-22 20:20:14 -0400

    added missing expect for placeholder className

--- test/components/Admin/Field.test.js
+++ test/components/Admin/Field.test.js
@@ -27,9 +27,11 @@ describe('<Field />', () => {
     const wrapper = shallow(<Field name='test' />);
     expect(wrapper.find('div')).to.have.length(1);
     expect(wrapper.find('div').prop('name')).to.equal('test');
+    expect(wrapper.find('div').prop('className')).to.equal('placeholder');
     expect(wrapper.find('div').text()).to.equal('test');
     wrapper.setProps({name: 'testing', id: 42, text: 'meaning', onClick: onClick});
     expect(wrapper.find('div').prop('name')).to.equal('testing');
+    expect(wrapper.find('div').prop('className')).to.equal(undefined);
     expect(wrapper.find('div').prop('id')).to.equal(42);
     expect(wrapper.find('div').text()).to.equal('meaning');
     expect(wrapper.find('div').prop('onClick')).to.equal(onClick);
