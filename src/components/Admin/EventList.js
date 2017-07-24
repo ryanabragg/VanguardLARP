d98e97511402fe3c8d1780838973395ea839b879
Ryan Bragg <ryan.a.bragg@gmail.com>
2017-07-24 15:14:47 -0400

    added EventList component for Admin

---
+++ src/components/Admin/EventList.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Event from './Event';
+import EventForm from './EventForm';
+
+class EventList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const rest = Object.assign({}, this.props);
+    delete rest.list;
+    delete rest.selected;
+    delete rest.onClick;
+    delete rest.onChange;
+    delete rest.onSubmit;
+    delete rest.onCancel;
+    delete rest.onDelete;
+    delete rest.scrollToForm;
+    return (
+      <article {...rest}>
+        {this.props.list.length == 0
+        ? null
+        : this.props.list.map(event => (
+          this.props.selected &&
+          event._id == this.props.selected._id
+          ? <EventForm
+              key={this.props.selected._id}
+              id={this.props.selected._id}
+              date={this.props.selected.date}
+              location={this.props.selected.location}
+              area={this.props.selected.area}
+              onChange={this.props.onChange}
+              onSubmit={this.props.onSubmit}
+              onCancel={this.props.onCancel}
+              onDelete={this.props.onDelete}
+              scrollToForm={this.props.scrollToForm}
+            />
+          : <Event
+              key={event._id}
+              id={event._id}
+              date={event.date}
+              location={event.location}
+              area={event.area}
+              onClick={this.props.onClick}
+            />
+        ))}
+      </article>
+    );
+  };
+}
+
+EventList.defaultProps = {
+  list: [],
+  scrollToForm: false
+}
+
+EventList.propTypes = {
+  list: PropTypes.array.isRequired,
+  selected: PropTypes.object,
+  onClick: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  onCancel: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
+  scrollToForm: PropTypes.bool
+};
+
+export default EventList;
---
+++ test/components/Admin/EventList.test.js
@@ -0,0 +1,216 @@
+import React from 'react';
+import { expect } from 'chai';
+import { spy } from 'sinon';
+import { shallow } from 'enzyme';
+import { JSDOM } from 'jsdom';
+
+import EventList from '../../../src/components/Admin/EventList';
+import EventForm from '../../../src/components/Admin/EventForm';
+import Event from '../../../src/components/Admin/Event';
+
+const window = (new JSDOM('<!doctype html><html><body></body></html>')).window;
+global.window = window;
+global.document = window.document;
+global.navigator = {
+  userAgent: 'node.js',
+};
+
+function copyProps(src, target) {
+  const props = Object.getOwnPropertyNames(src)
+    .filter(prop => typeof target[prop] === 'undefined')
+    .map(prop => Object.getOwnPropertyDescriptor(src, prop));
+  Object.defineProperties(target, props);
+}
+copyProps(window, global);
+
+describe('<EventList />', () => {
+  it('renders an article with an array of rule objects as Event components', () => {
+    const onClick = spy(),
+      onChange = spy(),
+      onSubmit = spy(),
+      onCancel = spy(),
+      onDelete = spy();
+    const wrapper = shallow(
+      <EventList
+        list={[]}
+        selected={{_id: ''}}
+        onClick={onClick}
+        onChange={onChange}
+        onSubmit={onSubmit}
+        onCancel={onCancel}
+        onDelete={onDelete}
+      />
+    );
+    expect(wrapper.find('article')).to.have.length(1);
+    expect(wrapper.find(EventForm)).to.have.length(0);
+    expect(wrapper.find(Event)).to.have.length(0);
+    wrapper.setProps({
+      list: [{
+        _id: '42',
+        date: '1979-10-12',
+        location: 'Galaxy'
+      }, {
+        _id: 'flux',
+        date: 'October 26, 1985',
+        location: 'Back',
+        area: 'Future'
+      }]
+    });
+    expect(wrapper.find('article')).to.have.length(1);
+    expect(wrapper.find(EventForm)).to.have.length(0);
+    expect(wrapper.find(Event)).to.have.length(2);
+  });
+
+  it('handles a blank selected prop', () => {
+    const onClick = spy(),
+      onChange = spy(),
+      onSubmit = spy(),
+      onCancel = spy(),
+      onDelete = spy();
+    const wrapper = shallow(
+      <EventList
+        list={[]}
+        onClick={onClick}
+        onChange={onChange}
+        onSubmit={onSubmit}
+        onCancel={onCancel}
+        onDelete={onDelete}
+      />
+    );
+    expect(wrapper.find('article')).to.have.length(1);
+    expect(wrapper.find(EventForm)).to.have.length(0);
+    expect(wrapper.find(Event)).to.have.length(0);
+    wrapper.setProps({
+      list: [{
+        _id: '42',
+        date: '1979-10-12',
+        location: 'Galaxy'
+      }, {
+        _id: 'flux',
+        date: 'October 26, 1985',
+        location: 'Back',
+        area: 'Future'
+      }]
+    });
+    expect(wrapper.find('article')).to.have.length(1);
+    expect(wrapper.find(EventForm)).to.have.length(0);
+    expect(wrapper.find(Event)).to.have.length(2);
+  });
+
+  it('renders a EventForm instead of a Event for the array object matching the selected object id', () => {
+    const onClick = spy(),
+      onChange = spy(),
+      onSubmit = spy(),
+      onCancel = spy(),
+      onDelete = spy();
+    const wrapper = shallow(
+      <EventList
+        list={[]}
+        selected={{_id: ''}}
+        onClick={onClick}
+        onChange={onChange}
+        onSubmit={onSubmit}
+        onCancel={onCancel}
+        onDelete={onDelete}
+      />
+    );
+    expect(wrapper.find('article')).to.have.length(1);
+    expect(wrapper.find(EventForm)).to.have.length(0);
+    expect(wrapper.find(Event)).to.have.length(0);
+    wrapper.setProps({
+      list: [{
+        _id: '42',
+        date: '1979-10-12',
+        location: 'Galaxy'
+      }, {
+        _id: 'flux',
+        date: 'October 26, 1985',
+        location: 'Back',
+        area: 'Future'
+      }],
+      selected: {
+        _id: '42',
+        date: '1979-10-12',
+        location: 'Galaxy'
+      }
+    });
+    expect(wrapper.find('article')).to.have.length(1);
+    expect(wrapper.find(EventForm)).to.have.length(1);
+    expect(wrapper.find(Event)).to.have.length(1);
+  });
+
+  it('passes the correct props to each Event', () => {
+    const onClick = spy(),
+      onChange = spy(),
+      onSubmit = spy(),
+      onCancel = spy(),
+      onDelete = spy();
+    const wrapper = shallow(
+      <EventList
+        list={[]}
+        selected={{_id: ''}}
+        onClick={onClick}
+        onChange={onChange}
+        onSubmit={onSubmit}
+        onCancel={onCancel}
+        onDelete={onDelete}
+      />
+    );
+    wrapper.setProps({
+      list: [{
+        _id: '42',
+        date: '1979-10-12',
+        location: 'Galaxy',
+        area: 'Hitchhiking'
+      }],
+    });
+    expect(wrapper.find(Event).prop('id')).to.equal('42');
+    expect(wrapper.find(Event).prop('date')).to.equal('1979-10-12');
+    expect(wrapper.find(Event).prop('location')).to.equal('Galaxy');
+    expect(wrapper.find(Event).prop('area')).to.equal('Hitchhiking');
+    expect(wrapper.find(Event).prop('onClick')).to.equal(onClick);
+  });
+
+  it('passes the correct props to the EventForm', () => {
+    const onClick = spy(),
+      onChange = spy(),
+      onSubmit = spy(),
+      onCancel = spy(),
+      onDelete = spy();
+    const wrapper = shallow(
+      <EventList
+        list={[]}
+        selected={{_id: ''}}
+        onClick={onClick}
+        onChange={onChange}
+        onSubmit={onSubmit}
+        onCancel={onCancel}
+        onDelete={onDelete}
+      />
+    );
+    wrapper.setProps({
+      list: [{
+        _id: '42',
+        date: 'these are not used',
+        location: 'these are not used',
+        area: 'these are not used'
+      }],
+      selected: {
+        _id: '42',
+        date: '1979-10-12',
+        location: 'Galaxy',
+        area: 'Hitchhiking'
+      },
+      scrollToForm: true
+    });
+    expect(wrapper.find(EventForm).prop('id')).to.equal('42');
+    expect(wrapper.find(EventForm).prop('date')).to.equal('1979-10-12');
+    expect(wrapper.find(EventForm).prop('location')).to.equal('Galaxy');
+    expect(wrapper.find(EventForm).prop('area')).to.equal('Hitchhiking');
+    expect(wrapper.find(EventForm).prop('onChange')).to.equal(onChange);
+    expect(wrapper.find(EventForm).prop('onSubmit')).to.equal(onSubmit);
+    expect(wrapper.find(EventForm).prop('onCancel')).to.equal(onCancel);
+    expect(wrapper.find(EventForm).prop('onDelete')).to.equal(onDelete);
+    expect(wrapper.find(EventForm).prop('scrollToForm')).to.equal(true);
+  });
+});
