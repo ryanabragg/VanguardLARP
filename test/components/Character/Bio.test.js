b5421e0a1a340590130bf285669f51cf1393a88a
Ryan Bragg <ryan.a.bragg@gmail.com>
2017-08-07 18:58:18 -0400

    forgot to include in commit 0dce309edac635042a83dffc0ac27493847bdf22

--- test/components/Character/Bio.test.js
+++ test/components/Character/Bio.test.js
@@ -23,8 +23,8 @@ copyProps(window, global);
 
 describe('<Bio />', () => {
   it('renders a div with the player name from props', () => {
-    const editCharacter = spy(), editRace = spy();
-    const wrapper = mount(<Bio player='test' editCharacter={editCharacter} editRace={editRace}/>);
+    const editCharacter = spy();
+    const wrapper = mount(<Bio player='test' editCharacter={editCharacter}/>);
     expect(wrapper.find('[data-character="player-name"]')).to.have.length(1);
     expect(wrapper.find('[data-character="player-name"]').type()).to.equal('div');
     expect(wrapper.find('[data-character="player-name"]').find('label').text()).to.equal('Player');
@@ -36,8 +36,8 @@ describe('<Bio />', () => {
   });
 
   it('renders a div with the character name from props', () => {
-    const editCharacter = spy(), editRace = spy();
-    const wrapper = mount(<Bio name='test' editCharacter={editCharacter} editRace={editRace}/>);
+    const editCharacter = spy();
+    const wrapper = mount(<Bio name='test' editCharacter={editCharacter}/>);
     expect(wrapper.find('[data-character="character-name"]')).to.have.length(1);
     expect(wrapper.find('[data-character="character-name"]').type()).to.equal('div');
     expect(wrapper.find('[data-character="character-name"]').find('label').text()).to.equal('Character');
@@ -49,36 +49,62 @@ describe('<Bio />', () => {
   });
 
   it('calls the editCharacter prop with the correct args when the character name is changed', () => {
-    const editCharacter = spy(), editRace = spy();
-    const wrapper = mount(<Bio name='test' editCharacter={editCharacter} editRace={editRace}/>);
+    const editCharacter = spy();
+    const wrapper = mount(<Bio name='test' editCharacter={editCharacter}/>);
     wrapper.find('[data-character="character-name"]').find('input').simulate('change', {target: {name: 'name', value: 'testing'}});
     expect(editCharacter.calledOnce).to.equal(true);
     expect(editCharacter.firstCall.args[0]).to.deep.equal({ type: 'NAME', data: 'testing' });
   });
 
-  it('renders a div with the character race from props', () => {
-    const editCharacter = spy(), editRace = spy();
-    const wrapper = mount(<Bio name='test' race='angel' editCharacter={editCharacter} editRace={editRace}/>);
+  it('renders a div with the character race from props as a select', () => {
+    const editCharacter = spy();
+    const wrapper = mount(<Bio name='test' race='angel' editCharacter={editCharacter}/>);
     expect(wrapper.find('[data-character="race"]')).to.have.length(1);
     expect(wrapper.find('[data-character="race"]').type()).to.equal('div');
     expect(wrapper.find('[data-character="race"]').find('label').text()).to.equal('Race');
     expect(wrapper.prop('race')).to.equal('angel');
-    expect(wrapper.find('[data-character="race"]').find('input').prop('value')).to.equal('angel');
+    expect(wrapper.find('[data-character="race"]').find('select').prop('value')).to.equal('angel');
     wrapper.setProps({race: 'demon'});
     expect(wrapper.prop('race')).to.equal('demon');
-    expect(wrapper.find('[data-character="race"]').find('input').prop('value')).to.equal('demon');
+    expect(wrapper.find('[data-character="race"]').find('select').prop('value')).to.equal('demon');
   });
 
-  it('calls the editRace prop when the race div is clicked on', () => {
-    const editCharacter = spy(), editRace = spy();
-    const wrapper = mount(<Bio name='test' race='angel' editCharacter={editCharacter} editRace={editRace}/>);
-    wrapper.find('[data-character="race"]').simulate('click');
-    expect(editRace.calledOnce).to.equal(true);
+  it('renders a race option per string in the races prop array');
+
+  it('calls the editCharacter prop with the correct args when the race is changed', () => {
+    const editCharacter = spy();
+    const wrapper = mount(<Bio name='test' editCharacter={editCharacter}/>);
+    wrapper.find('[data-character="race"]').find('select').simulate('change', {target: {name: 'race', value: 'testing'}});
+    expect(editCharacter.calledOnce).to.equal(true);
+    expect(editCharacter.firstCall.args[0]).to.deep.equal({ type: 'RACE', data: 'testing' });
+  });
+
+  it('renders a div with the character culture from props as a select', () => {
+    const editCharacter = spy();
+    const wrapper = mount(<Bio name='test' culture='angelic' editCharacter={editCharacter}/>);
+    expect(wrapper.find('[data-character="culture"]')).to.have.length(1);
+    expect(wrapper.find('[data-character="culture"]').type()).to.equal('div');
+    expect(wrapper.find('[data-character="culture"]').find('label').text()).to.equal('Culture');
+    expect(wrapper.prop('culture')).to.equal('angelic');
+    expect(wrapper.find('[data-character="culture"]').find('select').prop('value')).to.equal('angelic');
+    wrapper.setProps({culture: 'demonic'});
+    expect(wrapper.prop('culture')).to.equal('demonic');
+    expect(wrapper.find('[data-character="culture"]').find('select').prop('value')).to.equal('demonic');
+  });
+
+  it('renders a race option per string in the cultures prop array');
+
+  it('calls the editCharacter prop with the correct args when the race is changed', () => {
+    const editCharacter = spy();
+    const wrapper = mount(<Bio name='test' editCharacter={editCharacter}/>);
+    wrapper.find('[data-character="culture"]').find('select').simulate('change', {target: {name: 'culture', value: 'testing'}});
+    expect(editCharacter.calledOnce).to.equal(true);
+    expect(editCharacter.firstCall.args[0]).to.deep.equal({ type: 'CULTURE', data: 'testing' });
   });
 
   it('renders a div with the player build from props', () => {
-    const editCharacter = spy(), editRace = spy();
-    const wrapper = mount(<Bio name='test' playerBuild={42} editCharacter={editCharacter} editRace={editRace}/>);
+    const editCharacter = spy();
+    const wrapper = mount(<Bio name='test' playerBuild={42} editCharacter={editCharacter}/>);
     expect(wrapper.find('[data-character="build-player"]')).to.have.length(1);
     expect(wrapper.find('[data-character="build-player"]').type()).to.equal('div');
     expect(wrapper.find('[data-character="build-player"]').find('label').text()).to.equal('Player Build');
@@ -90,8 +116,8 @@ describe('<Bio />', () => {
   });
 
   it('renders a div with the character build total from props', () => {
-    const editCharacter = spy(), editRace = spy();
-    const wrapper = mount(<Bio name='test' build={42} editCharacter={editCharacter} editRace={editRace}/>);
+    const editCharacter = spy();
+    const wrapper = mount(<Bio name='test' build={42} editCharacter={editCharacter}/>);
     expect(wrapper.find('[data-character="build-total"]')).to.have.length(1);
     expect(wrapper.find('[data-character="build-total"]').type()).to.equal('div');
     expect(wrapper.find('[data-character="build-total"]').find('label').text()).to.equal('Build Total');
@@ -103,8 +129,8 @@ describe('<Bio />', () => {
   });
 
   it('renders a div with the character build spent from props', () => {
-    const editCharacter = spy(), editRace = spy();
-    const wrapper = mount(<Bio name='test' spent={42} editCharacter={editCharacter} editRace={editRace}/>);
+    const editCharacter = spy();
+    const wrapper = mount(<Bio name='test' spent={42} editCharacter={editCharacter}/>);
     expect(wrapper.find('[data-character="build-spent"]')).to.have.length(1);
     expect(wrapper.find('[data-character="build-spent"]').type()).to.equal('div');
     expect(wrapper.find('[data-character="build-spent"]').find('label').text()).to.equal('Build Spent');
@@ -116,8 +142,8 @@ describe('<Bio />', () => {
   });
 
   it('renders a div with the character level from props', () => {
-    const editCharacter = spy(), editRace = spy();
-    const wrapper = mount(<Bio name='test' level={1} editCharacter={editCharacter} editRace={editRace}/>);
+    const editCharacter = spy();
+    const wrapper = mount(<Bio name='test' level={1} editCharacter={editCharacter}/>);
     expect(wrapper.find('[data-character="level"]')).to.have.length(1);
     expect(wrapper.find('[data-character="level"]').type()).to.equal('div');
     expect(wrapper.find('[data-character="level"]').find('label').text()).to.equal('Level');
@@ -129,8 +155,8 @@ describe('<Bio />', () => {
   });
 
   it('renders a div with the character body from props', () => {
-    const editCharacter = spy(), editRace = spy();
-    const wrapper = mount(<Bio name='test' body={10} editCharacter={editCharacter} editRace={editRace}/>);
+    const editCharacter = spy();
+    const wrapper = mount(<Bio name='test' body={10} editCharacter={editCharacter}/>);
     expect(wrapper.find('[data-character="body"]')).to.have.length(1);
     expect(wrapper.find('[data-character="body"]').type()).to.equal('div');
     expect(wrapper.find('[data-character="body"]').find('label').text()).to.equal('Body');
@@ -142,8 +168,8 @@ describe('<Bio />', () => {
   });
 
   it('renders a div with the character buffs from props', () => {
-    const editCharacter = spy(), editRace = spy();
-    const wrapper = mount(<Bio name='test' buffs={3} editCharacter={editCharacter} editRace={editRace}/>);
+    const editCharacter = spy();
+    const wrapper = mount(<Bio name='test' buffs={3} editCharacter={editCharacter}/>);
     expect(wrapper.find('[data-character="buffs"]')).to.have.length(1);
     expect(wrapper.find('[data-character="buffs"]').type()).to.equal('div');
     expect(wrapper.find('[data-character="buffs"]').find('label').text()).to.equal('Buffs');
@@ -155,8 +181,8 @@ describe('<Bio />', () => {
   });
 
   it('renders a div with the character inscriptions from props', () => {
-    const editCharacter = spy(), editRace = spy();
-    const wrapper = mount(<Bio name='test' inscriptions={1} editCharacter={editCharacter} editRace={editRace}/>);
+    const editCharacter = spy();
+    const wrapper = mount(<Bio name='test' inscriptions={1} editCharacter={editCharacter}/>);
     expect(wrapper.find('[data-character="inscriptions"]')).to.have.length(1);
     expect(wrapper.find('[data-character="inscriptions"]').type()).to.equal('div');
     expect(wrapper.find('[data-character="inscriptions"]').find('label').text()).to.equal('Tattoos');
