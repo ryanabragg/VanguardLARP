65a156451258bb176dc477b6f55afc2a6740a684
Ryan Bragg <ryan.a.bragg@gmail.com>
2017-08-16 21:11:02 -0400

    improved admin menu

--- src/components/Admin/Menu.js
+++ src/components/Admin/Menu.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 
-import Navigation from '../styled/Navigation';
+import app from '../../util/feathersApp';
+
+import Navigation from './styled/Navigation';
 
 import routes from '../../routes';
 
@@ -9,13 +11,20 @@ class Menu extends React.Component {
   constructor (props) {
     super(props);
 
-    this.linkTo = routes.filter(route => route.slice(0, 6) === '/admin' && route.slice(route.length - 4, route.length) != '/:id');
+    this.links = routes.filter(route => route.slice(0, 6) === '/admin' && route.slice(route.length - 4, route.length) != '/:id');
 
     this.state = {
+      user: {},
       isMenuHidden: true
     };
 
     this.toggleMenu = this.toggleMenu.bind(this);
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
+  }
+
+  componentDidMount() {
+    app.auth().then(user => this.setState({user: user}));
   }
 
   toggleMenu(){
@@ -24,26 +33,46 @@ class Menu extends React.Component {
     });
   }
 
+  login(name, pass) {
+    app.login(name, pass).then(user => this.setState({user: user}));
+  }
+
+  logout() {
+    app.logout();
+    this.setState({user: {}});
+  }
+
   render() {
     return (
-      <Navigation isMenuHidden={this.state.isMenuHidden}>
-        <span className='menu-logo'><img src='../logo.svg'/></span>
-        {this.linkTo.map((link, index) => {
+      <Navigation>
+        <div className='menu-toggle' onClick={this.toggleMenu}></div>
+        <div className='menu-logo'><img src='../logo.svg'/></div>
+        {this.state.isMenuHidden
+        ? (<div className='menu-current'>
+            {(this.props.location.pathname.charAt(7).toUpperCase() + this.props.location.pathname.slice(8)) || 'Dashboard'}
+          </div>)
+        : null}
+        {this.links.map((link, index) => {
           let dashboard = link === '/admin';
-          let name = dashboard ? 'dashboard' : link.slice(7, link.length);
-          name = name.charAt(0).toUpperCase() + name.substr(1).toLowerCase();
+          let name = dashboard ? 'dashboard' : link.slice(7);
           return (
-            <NavLink
-              key={index}
-              exact={dashboard}
-              to={link}
-              activeClassName='nav-active'
-            >
-              {name}
-            </NavLink>
+            <div className={'menu-item' + (this.state.isMenuHidden ? '' : ' show')} key={index}>
+              <NavLink
+                exact={dashboard}
+                to={link}
+                activeClassName='nav-active'
+              >
+                {name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()}
+              </NavLink>
+            </div>
           );
         })}
-        <span className='menu-toggle' onClick={this.toggleMenu}>&#9776;</span>
+        <div className='menu-user'>
+          {this.state.user
+          ? <div onClick={this.login}>Login</div>
+          : <div onClick={this.logout}>Logout</div>
+          }
+        </div>
       </Navigation>
     );
   }
--- src/components/Admin/styled/Navigation.js
+++ src/components/Admin/styled/Navigation.js
@@ -2,13 +2,11 @@ import styled from 'styled-components';
 import Color from 'color';
 
 const Navigation = styled.nav`
-  overflow: hidden;
-  background: ${props => Color(props.theme.colors.primary).grayscale().hex()};
+  display: block;
   width: 100%;
-  margin-bottom: 10px;
+  padding: 5px 0 5px 0;
+  margin: 0 0 10px 0;
   font-variant: small-caps;
-  text-align: center;
-  line-height: 60px;
   font-size: 2em;
   font-family: ${props => props.theme.font.trebuchet};
   color: ${props => {
@@ -17,47 +15,54 @@ const Navigation = styled.nav`
     let alpha = background.light() ? props.theme.alphaDarkText.primary : props.theme.alphaLightText.primary;
     return base.mix(background, alpha).hex();
   }};
+  background: ${props => Color(props.theme.colors.primary).grayscale().hex()};
   a, span {
-    float: left;
-    height: 100%;
-    padding: 0 0.5em 0 0.5em;
     text-decoration: none;
     color: inherit;
   }
   a:hover {
-    background: ${props => Color(props.theme.colors.primary).grayscale().darken(0.5).hex()};
     color: ${props => props.theme.colors.secondary};
   }
   a.nav-active {
     color: ${props => props.theme.colors.secondary};
   }
   img {
+    display: inline-block;
     height: 40px;
     vertical-align: text-bottom;
   }
-  .menu-toggle {
-    position: absolute;
-    top: 0;
-    right: 0;
-    height: 100%;
+  div.menu-item, div.menu-current, div.menu-logo, div.menu-user, div.menu-divider {
+    display: inline-block;
     padding: 0 0.5em 0 0.5em;
+  }
+  div.menu-current {
     display: none;
   }
+  div.menu-toggle {
+    display: none;
+    position: absolute;
+    top: 15px;
+    right: 10px;
+    width: 0;
+    height: 0;
+    border-left: 10px solid transparent;
+    border-right: 10px solid transparent;
+    border-top: 10px solid #FFF;
+  }
+  div.menu-toggle:hover {
+    border-top: 10px solid ${props => props.theme.colors.secondary};
+    cursor: pointer;
+  }
   @media (max-width: ${props => props.theme.breakpoints.s}px) {
-    .menu-logo {
-      padding-right: ${props => props.isMenuHidden ? '0.5em' : '100%'};
+    div.menu-item, div.menu-user, div.menu-divider {
+      display: none;
     }
-    a {
-      float: none;
-      display: ${props => props.isMenuHidden ? 'none' : 'block'};
-      text-align: left;
-    }
-    a.nav-active {
-      float: left;
+    div.show {
       display: block;
     }
-    .menu-toggle {
-      display: block;
+    div.menu-current, div.menu-toggle {
+      display: inline-block;
+      padding: 0;
     }
   }
 `;
