0cd856e02658e857378aead27fb8c5953896537c
Ryan Bragg <ryan.a.bragg@gmail.com>
2017-09-26 15:20:52 -0400

    Enter key press on password field triggers login

--- src/components/User/Login.js
+++ src/components/User/Login.js
@@ -20,6 +20,8 @@ class Login extends React.Component {
     this.loginLocal = this.loginLocal.bind(this);
 
     this.handleFormInputChange = this.handleFormInputChange.bind(this);
+    this.handleFormKeyPress = this.handleFormKeyPress.bind(this);
+
     this.handleFormLoginFacebook = this.handleFormLoginFacebook.bind(this);
     this.handleFormLoginLocal = this.handleFormLoginLocal.bind(this);
     this.handleFormPasswordRecovery = this.handleFormPasswordRecovery.bind(this);
@@ -55,6 +57,17 @@ class Login extends React.Component {
       nextState[target.name] = target.type === 'checkbox' ? target.checked : target.value;
       return nextState;
     });
+  }
+
+  handleFormKeyPress(e) {
+    if(e.key != 'Enter')
+      return;
+    if(this.state.view == 'login')
+      this.handleFormLoginLocal(e);
+    else if(this.state.view == 'register')
+      this.handleFormRegisterLocal(e);
+    else if(this.state.view == 'password-recovery')
+      this.handleFormPasswordRecovery(e);
   }
 
   handleFormLoginFacebook(e) {
@@ -123,6 +136,7 @@ class Login extends React.Component {
           name='password'
           value={this.state.password}
           onChange={this.handleFormInputChange}
+          onKeyPress={this.handleFormKeyPress}
         />
         <button type='button' value="submit" onClick={this.handleFormLoginLocal}>Sign In</button>
         <span className='option-left' onClick={this.handleViewPasswordRecovery}>Forgot your password?</span>
@@ -152,6 +166,7 @@ class Login extends React.Component {
           name='password'
           value={this.state.password}
           onChange={this.handleFormInputChange}
+          onKeyPress={this.handleFormKeyPress}
         />
         <button type='button' value="submit" onClick={this.handleFormRegisterLocal}>Sign Up</button>
         <span className='option-right' onClick={this.handleViewLogin}>Sign In</span>
@@ -170,6 +185,7 @@ class Login extends React.Component {
           name='email'
           value={this.state.email}
           onChange={this.handleFormInputChange}
+          onKeyPress={this.handleFormKeyPress}
         />
         <button type='button' value="submit" onClick={this.handleFormPasswordRecovery}>Reset Password</button>
         <span className='option-left' onClick={this.handleViewLogin}>Sign In</span>
